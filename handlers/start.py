2bd950342afd61e09266e66c823271fbc225e4e0
Rodinchuk <rodandser@gmail.com>
2025-05-09 16:37:23 +0300

    feedbacks commit

--- handlers/start.py
+++ handlers/start.py
@@ -18,7 +18,7 @@ async def start_handler(message: types.Message):
     is_registered = user_data is not None and user_data.get("registered", False)
 
     if is_registered:
-        first_name = user_data.get("first_name", "").split()[0] if user_data.get("first_name") else ""
+        first_name = user_data.get("name", "").split()[0] if user_data.get("name") else ""
         await message.answer(
             f"Радий знову тебе бачити, {first_name}! 👋\n"
             "Зараз на панелі ти бачиш розділи – тисни на них, щоб дізнатись більше деталей про кожен блок 🔎.",
