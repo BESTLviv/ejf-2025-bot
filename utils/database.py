091aec8fb32c3e55b602a60f7e7006bc5e45446d
Rodinchuk <rodandser@gmail.com>
2025-05-11 00:51:57 +0300

    changed cv adding logic

--- utils/database.py
+++ utils/database.py
@@ -36,14 +36,23 @@ async def add_user(user_data: dict):
 async def get_user(user_id: int):
     return await users_collection.find_one({"telegram_id": user_id})
 
-async def add_cv(user_id: int, cv_file_path: str = None):
-    user = await users_collection.find_one({"telegram_id": user_id})
-    cv_data = {
-        "telegram_id": user_id,
-        "user_name": user["name"] if user else str(user_id),
-        "cv_file_path": cv_file_path,
-    }
-    await cv_collection.insert_one(cv_data)
+async def add_cv(user_id: int, cv_file_path: str = None, position: str = None, 
+                     languages: list = None, education: str = None, experience: str = None, 
+                     skills: list = None, about: str = None, contacts: dict = None):
+        user = await users_collection.find_one({"telegram_id": user_id})
+        cv_data = {
+            "telegram_id": user_id,
+            "user_name": user["name"] if user else str(user_id),
+            "cv_file_path": cv_file_path,
+            "position": position,
+            "languages": languages,
+            "education": education,
+            "experience": experience,
+            "skills": skills,
+            "about": about,
+            "contacts": contacts
+        }
+        await cv_collection.insert_one(cv_data)
 
 async def get_cv(user_id: int):
     return await cv_collection.find_one({"telegram_id": user_id})
