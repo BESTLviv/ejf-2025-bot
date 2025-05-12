c3e93ba03f0356c4f43d9e2272ae28e47b411a89
Rodinchuk <rodandser@gmail.com>
2025-05-12 23:56:51 +0300

    registration changes

--- handlers/cv.py
+++ handlers/cv.py
@@ -429,7 +429,8 @@ async def change_existing_cv(message: types.Message, state: FSMContext):
     await message.answer("Чудово, давай почнемо створення нового CV!")
     await state.clear()
     await state.set_state(CVStates.position)
-    await message.answer("Тож почнімо знову, яка посада або напрям тебе цікавить? Наприклад: стажування в сфері Data Science, робота інженером-проєктувальником тощо.")
+    await message.answer("Тож почнімо знову, яка посада або напрям тебе цікавить? Наприклад: стажування в сфері Data Science, робота інженером-проєктувальником тощо.",
+                         reply_markup=back2menu_kb())
             
 
 
